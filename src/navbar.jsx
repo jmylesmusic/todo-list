ac12946e80c1c97ab4734472ffeb630640101b28
James Myles <myles.james@gmail.com>
2024-04-01 18:01:12 +0200

    "Finally not broken."

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,8 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-
 const Navbar = () => {
   return (
     <div className="navbar">
