3dce732908deb04632b79c19f59525152786b574
James Myles <myles.james@gmail.com>
2024-04-01 17:00:35 +0200

    "navbar and footer JSX"

---
+++ vite-project/src/footer.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function Footer() {
+  <div className="footer">
+    View this page on{" "}
+    <a href="https://github.com/jmylesmusic/todo-list">Github</a>
+  </div>;
+}
---
+++ vite-project/src/navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+function navbar() {
+  <div className="navbar">
+    <div>Task:</div>
+    <div>Description:</div>
+    <div>Completed?</div>
+  </div>;
+}
