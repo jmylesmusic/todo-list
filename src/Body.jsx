d3a9d6fc73f4fb7b56d0ffb6626c105403d33ffc
James Myles <myles.james@gmail.com>
2024-04-02 18:02:02 +0200

    List with delete buttons

--- src/Body.jsx
+++ src/Body.jsx
@@ -2,17 +2,25 @@ import { useState } from "react";
 import toDoListItems from "./assets/todolist-items.json";
 
 function Body() {
-  const [item, setItem] = useState(toDoListItems);
-
+  const [task, setTask] = useState(toDoListItems);
+  const deleteTask = (itemId) => {
+    const copy = JSON.parse(JSON.stringify(task));
+    const filteredTasks = copy.filter((theitem) => {
+      return theitem.id !== itemId;
+    });
+    setTask(filteredTasks);
+  };
   return (
     <div>
-      {item.map((task) => {
+      {task.map((task) => {
         return (
-          <div key={task.id}>
+          <div key={task.id} className="info-table">
             <p>{task.item}</p>
             <p>{task.priority}</p>
             <p>{task.due_date}</p>
-            <button>Delete</button>
+            <button onClick={() => deleteTask(task.id)} className="btn-delete">
+              Delete
+            </button>
           </div>
         );
       })}
