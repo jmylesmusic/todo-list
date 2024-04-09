37c94c31bae3b74a4febebdbd9d7b54e92bcca5c
Ricardo Watson <Rickangelodan31@gmail.com>
2024-04-09 02:39:42 +0200

    i added a new list to be adedd and remove next step is to make the screen change on media and the sidebar hide

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,48 @@ import Body from "./components/Body.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+    setList([...list, newTodo]);
+
+    setInput("");
+  };
+
+  const deleteTodo = (id) => {
+    const newList = list.filter((todo) => todo.id !== id);
+
+    setList(newList);
+  };
   // return
   return (
     <div>
+      <h1>Todo List</h1>
+      <div className="todosearch">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={() => addTodo(input)}>add</button>
+      </div>
+
+      <div className="todoadded">
+        <ul>
+          {list.map((todo) => (
+            <li key={todo.id}>
+              {todo.todo}
+              <button onClick={() => deleteTodo(todo.id)}>&times;</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
       <Navbar />
-      <div className="doing">To-Do:</div>
       <Sidebar />
       <Body />
       <Footer />
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,40 +2,42 @@ import { useState } from "react";
 import toDoListItems from "../assets/todolist-items.json";
 
 function Body() {
-  const [task, setTask] = useState(toDoListItems);
-  const deleteTask = (itemId) => {
-    const copy = JSON.parse(JSON.stringify(task));
-    const filteredTasks = copy.filter((theitem) => {
-      return theitem.id !== itemId;
-    });
-    setTask(filteredTasks);
-  };
-  return (
-    <div>
-      {task.map((task) => {
-        return (
-          <div key={task.id} className="info-table">
-            <div className="navbar">
-              <div id="navbar-item"></div>
-              <div id="navbar-item">Task:</div>
-              <div>{task.item}</div>
-              <div id="navbar-item">Priority:</div>
-              <div>{task.priority}</div>
-              <div id="navbar-item">Due Date:</div>
-              <div>{task.due_date}</div>
-              <div id="navbar-item">Completed?</div>
-              <button
-                onClick={() => deleteTask(task.id)}
-                className="btn-delete"
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+  // const [task, setTask] = useState(toDoListItems);
+  // const deleteTask = (itemId) => {
+  //   const copy = JSON.parse(JSON.stringify(task));
+  //   const filteredTasks = copy.filter((theitem) => {
+  //     return theitem.id !== itemId;
+  //   });
+  //   setTask(filteredTasks);
+  // };
+  // return (
+  //   <div className="container">
+  //     {task.map((task) => {
+  //       return (
+  //         <div key={task.id} className="info-table">
+  //           <div className="list-heading-ctn">
+  //             <div id="navbar-item"></div>
+  //             <div id="navbar-item">Task:</div>
+  //             <div id="navbar-item">Priority:</div>
+  //             <div id="navbar-item">Due Date:</div>
+  //             <div id="navbar-item">Completed?</div>
+  //           </div>
+  //           <div className="list-item-ctn">
+  //             <div>{task.item}</div>
+  //             <div>{task.priority}</div>
+  //             <div>{task.due_date}</div>
+  //             <button
+  //               onClick={() => deleteTask(task.id)}
+  //               className="btn-delete"
+  //             >
+  //               Delete
+  //             </button>
+  //           </div>
+  //         </div>
+  //       );
+  //     })}
+  //   </div>
+  // );
 }
 
 export default Body;
